a9dfd9ceebc8e7e86c0c8669d63fca7cfb60cc84
Eugene-Kuts <eugene.kuts@gmail.com>
2019-07-24 23:50:50 +0300

    ДЗ№3 Изменения в соответствии с комментариями

--- hw03-TestFrameWork/src/main/java/ru/otus/hw03/TestRunner.java
+++ hw03-TestFrameWork/src/main/java/ru/otus/hw03/TestRunner.java
@@ -1,56 +1,79 @@
 package ru.otus.hw03;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestRunner {
-    public static void main(String[] args){
-        Class<TestClass> testClass = TestClass.class;
-        runTests(testClass);
+    private static int tests = 0;
+    private static int passed = 0;
+    private static Method after = null;
+    private static Method before = null;
+    private static List<Method> testMethodsList = new ArrayList<>();
+    private static Class testClass = null;
+
+    public static void runTests(String testClassName) {
+        findClass(testClassName); //ищем класс
+        findAnnotatedMethods(); //ищем аннотированные методы
+        runAnnotatedMethods(); // выполняем аннотированные методы
+
+        System.out.printf("Всего тестов: " + tests + ", Успешных: " + passed + ", Неуспешных: " + (tests - passed));
     }
 
-    public static void runTests(Class testClass) {
-        if(testClass.getDeclaredMethods().length == 0){
-            throw new RuntimeException("Нет ни одно метода для теста");
-        }
-        int tests = 0;
-        int passed = 0;
-        Method after = testClass.getDeclaredMethods()[0];
-        Method before = testClass.getDeclaredMethods()[0];
-
-        for (Method m1 : testClass.getDeclaredMethods()) {
-            if (m1.isAnnotationPresent(After.class)) {
-                after = m1;
-            }
-        }
-
-        for (Method m2 : testClass.getDeclaredMethods()) {
-            if (m2.isAnnotationPresent(Before.class)) {
-                before = m2;
-            }
-        }
+    private static void findAnnotatedMethods(){
 
         for (Method m : testClass.getDeclaredMethods()) {
             if (m.isAnnotationPresent(Test.class)) {
-                tests++;
-                try {
-                    TestClass newTestClass = new TestClass();
-                    if(before.isAnnotationPresent(Before.class)) {//аннотации before может и не быть
-                        before.invoke(newTestClass);
-                    }
-
-                    m.invoke(newTestClass);
-
-                    if(after.isAnnotationPresent(After.class)) {//аннотации after может и не быть
-                        after.invoke(newTestClass);
-                    }
-                    passed++;
-                } catch (Exception e) {
-                    Throwable exception = e.getCause();
-                    System.out.println("Invalid method: " + m.getName() + " with annotation: "+ m.getDeclaredAnnotations()[0] + ". Method failed because: " + exception);
-                }
+                testMethodsList.add(m);
+            }else if(m.isAnnotationPresent(After.class)){
+                after = m;
+            }else if(m.isAnnotationPresent(Before.class)){
+                before = m;
             }
         }
-        System.out.printf("Passed: %d, Failed: %d%n",
-                passed, tests - passed);
     }
+
+    private static void runAnnotatedMethods(){
+    Object instance;
+        for(Method m : testMethodsList){
+            instance = createInstance(testClass);//для каждой тройки @Before @Test @After создаем свой instance
+            tests++;
+            invokeMethod(before,instance);
+            invokeMethod(m,instance);
+            invokeMethod(after,instance);
+        }
+    }
+
+    private static void findClass(String testClassName){
+        try {
+            testClass = (Class) Class.forName(testClassName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Object createInstance(Class classForInstance){
+        Object inst = new Object();
+        try {
+            inst = classForInstance.getDeclaredConstructors()[0].newInstance();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return inst;
+    }
+
+    private static void invokeMethod(Method m, Object ins){
+        if(m!=null) {
+            try {
+                m.invoke(ins);
+                if(m.isAnnotationPresent(Test.class)){
+                    passed++;
+                }
+            } catch (Exception e) {
+                Throwable exception = e.getCause();
+                System.out.println("Invalid method: " + m.getName() + " with annotation: "+ m.getDeclaredAnnotations()[0] + ". Method failed because: " + exception);
+            }
+        }
+    }
+
 }
