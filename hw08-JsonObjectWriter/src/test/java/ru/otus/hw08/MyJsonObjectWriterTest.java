4c736dbcd6345ff427fbdd62988f77007176e790
Eugene-Kuts <eugene.kuts@gmail.com>
2019-10-25 20:27:23 +0300

    ДЗ№8. Cвой json object writer Добавлены "прямые тесты"

--- hw08-JsonObjectWriter/src/test/java/ru/otus/hw08/MyJsonObjectWriterTest.java
+++ hw08-JsonObjectWriter/src/test/java/ru/otus/hw08/MyJsonObjectWriterTest.java
@@ -22,6 +22,12 @@ public class MyJsonObjectWriterTest {
         myJsonObjectWriter = new MyJsonObjectWriter();
     }
 
+    //проверка на null WithoutGson
+    @Test
+    public void nullObjectWithoutGson() throws IllegalAccessException {
+        assertEquals("null", myJsonObjectWriter.toJson(null));
+    }
+
     //проверка на null
     @Test
     public void nullObject() throws IllegalAccessException {
@@ -34,6 +40,19 @@ public class MyJsonObjectWriterTest {
         assertEquals(googleGson.toJson(5), myJsonObjectWriter.toJson(5));
     }
 
+    //integerWithoutGson
+    @Test
+    public void integerObjectWithoutGson() throws IllegalAccessException {
+        assertEquals("5", myJsonObjectWriter.toJson(5));
+    }
+
+    //integerArrayWithoutGson
+    @Test
+    public void integerArrayObjectWithoutGson() throws IllegalAccessException {
+        int[] array = {1, 2, 3};
+        assertEquals("[1,2,3]", myJsonObjectWriter.toJson(array));
+    }
+
     //integerArray
     @Test
     public void integerArrayObject() throws IllegalAccessException {
@@ -41,10 +60,23 @@ public class MyJsonObjectWriterTest {
         assertEquals(googleGson.toJson(array), myJsonObjectWriter.toJson(array));
     }
 
+    //booleanWithoutGson
+    @Test
+    public void booleanObjectWithoutGson() throws IllegalAccessException {
+        assertEquals("true", myJsonObjectWriter.toJson(true));
+    }
+
     //boolean
     @Test
     public void booleanObject() throws IllegalAccessException {
         assertEquals(googleGson.toJson(true), myJsonObjectWriter.toJson(true));
+    }
+
+    //booleanArrayWithoutGson
+    @Test
+    public void booleanArrayObjectWithoutGson() throws IllegalAccessException {
+        boolean[] array = {true, false, true};
+        assertEquals("[true,false,true]", myJsonObjectWriter.toJson(array));
     }
 
     //booleanArray
@@ -54,10 +86,23 @@ public class MyJsonObjectWriterTest {
         assertEquals(googleGson.toJson(array), myJsonObjectWriter.toJson(array));
     }
 
+    //doubleWithoutGson
+    @Test
+    public void doubleObjectWithoutGson() throws IllegalAccessException {
+        assertEquals("10.0", myJsonObjectWriter.toJson(10d));
+    }
+
     //double
     @Test
     public void doubleObject() throws IllegalAccessException {
         assertEquals(googleGson.toJson(10d), myJsonObjectWriter.toJson(10d));
+    }
+
+    //doubleArrayWithoutGson
+    @Test
+    public void doubleArrayObjectWithoutGson() throws IllegalAccessException {
+        double[] array = {10d, 20d, 30d};
+        assertEquals("[10.0,20.0,30.0]", myJsonObjectWriter.toJson(array));
     }
 
     //doubleArray
@@ -67,10 +112,22 @@ public class MyJsonObjectWriterTest {
         assertEquals(googleGson.toJson(array), myJsonObjectWriter.toJson(array));
     }
 
+    //floatWithoutGson
+    @Test
+    public void floatObjectWithoutGson() throws IllegalAccessException {
+        assertEquals("15.0", myJsonObjectWriter.toJson(15f));
+    }
+
     //float
     @Test
     public void floatObject() throws IllegalAccessException {
         assertEquals(googleGson.toJson(15f), myJsonObjectWriter.toJson(15f));
+    }
+
+    //StringWithoutGson
+    @Test
+    public void stringObjectWithoutGson() throws IllegalAccessException {
+        assertEquals("\"Success\"", myJsonObjectWriter.toJson("Success"));
     }
 
     //String
@@ -80,11 +137,24 @@ public class MyJsonObjectWriterTest {
         assertEquals(googleGson.toJson(str), myJsonObjectWriter.toJson(str));
     }
 
+    //StringArrayWithoutGson
+    @Test
+    public void stringArrayObjectWithoutGson() throws IllegalAccessException {
+        String[] array = {"Альфа", "Бета", "Гамма"};
+        assertEquals("[\"Альфа\",\"Бета\",\"Гамма\"]", myJsonObjectWriter.toJson(array));
+    }
+
     //StringArray
     @Test
     public void stringArrayObject() throws IllegalAccessException {
         String[] array = {"Альфа", "Бета", "Гамма"};
         assertEquals(googleGson.toJson(array), myJsonObjectWriter.toJson(array));
+    }
+
+    //CharacterWithoutGson
+    @Test
+    public void characterObjectWithoutGson() throws IllegalAccessException {
+        assertEquals("\"A\"", myJsonObjectWriter.toJson("A"));
     }
 
     //Character
@@ -93,10 +163,22 @@ public class MyJsonObjectWriterTest {
         assertEquals(googleGson.toJson("A"), myJsonObjectWriter.toJson("A"));
     }
 
+    //LongWithoutGson
+    @Test
+    public void longObjectWithoutGson() throws IllegalAccessException {
+        assertEquals("5", myJsonObjectWriter.toJson(5l));
+    }
+
     //Long
     @Test
     public void longObject() throws IllegalAccessException {
         assertEquals(googleGson.toJson(5l), myJsonObjectWriter.toJson(5l));
+    }
+
+    //ByteWithoutGson
+    @Test
+    public void byteObjectWithoutGson() throws IllegalAccessException {
+        assertEquals("55", myJsonObjectWriter.toJson((byte)55));
     }
 
     //Byte
@@ -105,16 +187,38 @@ public class MyJsonObjectWriterTest {
         assertEquals(googleGson.toJson((byte)55), myJsonObjectWriter.toJson((byte)55));
     }
 
+    //ShortWithoutGson
+    @Test
+    public void shortObjectWithoutGson() throws IllegalAccessException {
+        assertEquals("55", myJsonObjectWriter.toJson((short)55));
+    }
+
     //Short
     @Test
     public void shortObject() throws IllegalAccessException {
         assertEquals(googleGson.toJson((short)55), myJsonObjectWriter.toJson((short)55));
     }
 
+    //ArrayWithoutGson
+    @Test
+    public void arrayObjectWithoutGson() throws IllegalAccessException {
+        assertEquals("[1,2,3]", myJsonObjectWriter.toJson(new int[] {1, 2, 3}));
+    }
+
     //Array
     @Test
     public void arrayObject() throws IllegalAccessException {
         assertEquals(googleGson.toJson(new int[] {1, 2, 3}), myJsonObjectWriter.toJson(new int[] {1, 2, 3}));
+    }
+
+    //CollectionWithoutGson
+    @Test
+    public void collectionObjectWithoutGson() throws IllegalAccessException {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        assertEquals("[\"1\",\"2\",\"3\"]", myJsonObjectWriter.toJson(list));
     }
 
     //Collection
@@ -127,11 +231,26 @@ public class MyJsonObjectWriterTest {
         assertEquals(googleGson.toJson(list), myJsonObjectWriter.toJson(list));
     }
 
+    //SimpleObjectWithoutGson
+    @Test
+    public void simpleObjectWithoutGson() throws IllegalAccessException {
+        SimpleObject simpleObject = new SimpleObject(55, "Ivan");
+        assertEquals("{\"age\":55,\"name\":\"Ivan\"}", myJsonObjectWriter.toJson(simpleObject));
+    }
+
     //SimpleObject
     @Test
     public void simpleObject() throws IllegalAccessException {
         SimpleObject simpleObject = new SimpleObject(55, "Ivan");
         assertEquals(googleGson.toJson(simpleObject), myJsonObjectWriter.toJson(simpleObject));
+    }
+
+    //ComplexObjectWithoutGson
+    @Test
+    public void complexObjectWithoutGson() throws IllegalAccessException {
+        ComplexObject complexObject = new ComplexObject();
+        String json = myJsonObjectWriter.toJson(complexObject);
+        assertEquals("{\"stringField\":\"Happy Day\",\"byteField\":10,\"byteOField\":20,\"shortField\":30,\"shortOField\":40,\"intField\":50,\"integerOField\":60,\"longField\":70,\"longOField\":80,\"booleanField\":false,\"booleanOField\":true,\"charField\":\"z\",\"characterOField\":\"x\",\"floatField\":1.100000023841858,\"floatOField\":2.200000047683716,\"doubleField\":3.3,\"doubleOField\":4.4,\"array\":[1,2,3],\"list\":[\"Victory\"]}", json);
     }
 
     //ComplexObject
