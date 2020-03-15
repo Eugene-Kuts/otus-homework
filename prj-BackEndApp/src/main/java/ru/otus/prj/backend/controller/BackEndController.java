812717e0217cb57ce3dfd4a560a874a4d1f2d6ce
Eugene-Kuts <eugene.kuts@gmail.com>
2020-03-15 19:04:58 +0300

    Add Swagger UI
    Signed-off-by: Eugene-Kuts <eugene.kuts@gmail.com>

--- prj-BackEndApp/src/main/java/ru/otus/prj/backend/controller/BackEndController.java
+++ prj-BackEndApp/src/main/java/ru/otus/prj/backend/controller/BackEndController.java
@@ -2,6 +2,10 @@ package ru.otus.prj.backend.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -14,17 +18,23 @@ import ru.otus.prj.backend.controller.exceptions.NullValueParameterException;
 @Slf4j
 @RestController
 @AllArgsConstructor
+@Api(value="Back end operation", description="Operation of multiplication for incoming numbers")
 public class BackEndController {
 
-    @RequestMapping(path = "/", produces = "application/json", consumes = "application/json", method = RequestMethod.GET)
+    @ApiOperation(value = "Operation to get multiplication of incoming parameters", response = Iterable.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully multiplication"),
+            @ApiResponse(code = 404, message = "Bad Request"),
+            @ApiResponse(code = 500, message = "Something's wrong")})
+    @RequestMapping(path = "/getMultiplication", produces = "application/json", consumes = "application/json", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity<String> getBackEndResponse(@RequestBody BackEndRequest backEndRequest) throws NullValueParameterException {
+    public ResponseEntity<BackEndResponse> getBackEndResponse(@RequestBody BackEndRequest backEndRequest) throws NullValueParameterException {
         log.info("backEndRequest= " + backEndRequest);
         ObjectMapper mapper = new ObjectMapper();
         try {
-            String json = mapper.writeValueAsString(new BackEndResponse(backEndRequest.getMultiplication()));
+            String json = mapper.writeValueAsString(new BackEndResponse((backEndRequest.getParamA()*backEndRequest.getParamB()*backEndRequest.getParamC())));
             log.info("json= " + json);
-            return new ResponseEntity<>(json, HttpStatus.OK);
+            return new ResponseEntity(json, HttpStatus.OK);
         } catch (NullPointerException npe){
             throw new NullValueParameterException();
         } catch(JsonProcessingException ex) {
--- prj-BackEndApp/src/main/java/ru/otus/prj/backend/request/BackEndRequest.java
+++ prj-BackEndApp/src/main/java/ru/otus/prj/backend/request/BackEndRequest.java
@@ -1,5 +1,7 @@
 package ru.otus.prj.backend.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,16 +10,18 @@ import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
+@ApiModel(description = "Request of tree numbers")
 public class BackEndRequest {
 
+    @ApiModelProperty(notes = "Value of parameter A", example = "1", position = 1)
     @NotNull
     private Integer paramA;
+
+    @ApiModelProperty(notes = "Value of parameter B", example = "2", position = 2)
     @NotNull
     private Integer paramB;
+
+    @ApiModelProperty(notes = "Value of parameter C", example = "3", position = 3)
     @NotNull
     private Integer paramC;
-
-    public Integer getMultiplication() {
-        return paramA * paramB * paramC;
-    }
 }
